bf49a43aa4a4fb6a68399977c84b6006347d120d
Bradley Mattison <bradleymattison2000@gmail.com>
2025-07-24 10:44:05 +0100

    production debugging server erros unable to fetch questions

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -12,20 +12,34 @@ export async function GET(request: Request) {
     const amount = searchParams.get('amount') || '15';
     const difficulty = searchParams.get('difficulty');
 
+    console.log('API Activated:', { category, amount, difficulty });
+
     let url = `https://opentdb.com/api.php?amount=${amount}&category=${category}&type=multiple`
     
     if (difficulty && difficulty !== "undefined" && difficulty !== "mixed") {
         url += `&difficulty=${difficulty}`
     }
 
-    const res = await fetch(url)
+    let res;
 
+    try {
+        res = await fetch(url, { cache: 'no-store' });
+    } catch (err) {
+        console.error('Fetch failed:', err);
+        return NextResponse.json({ error: 'Fetch threw error' }, { status: 500 });
+    }
+
+    
     if (!res.ok) {
         console.error('Failed response:', res.status);
         return NextResponse.json({error: 'Failed to fetch trivia questions'}, {status: 500})
     }
-
+    
     const rawData: {results: RawTriviaQuestion[]} = await res.json()
+    
+    if (rawData.results.length === 0) {
+        return NextResponse.json({ error: 'No questions found' }, { status: 404 });
+    }
 
     const cleanedQuestions: CleanedQuestion[] = rawData.results.map(cleanQuestion);
 
