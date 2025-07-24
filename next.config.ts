b72655a568e1a5af79b6d9a5f3e4ab09a675e887
Bradley Mattison <bradleymattison2000@gmail.com>
2025-07-24 10:18:55 +0100

    trial and error stop eslint hell during build on vercel

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
